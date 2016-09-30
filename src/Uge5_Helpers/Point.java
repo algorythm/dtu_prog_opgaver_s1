4941bb2a73e3d4ebcdb4f95344e84c8f4750471c
Anders W. Olsen <awo@defalt.net>
2016-09-30 09:31:50 +0200

    Restructering the code

--- src/Uge5_Helpers/Point.java
+++ src/Uge5_Helpers/Point.java
@@ -4,16 +4,12 @@ package Uge5_Helpers;
  * Created by AndersWOlsen on 25-09-2016.
  */
 public class Point {
-    private int    x;
-    private int    y;
+    private int x;
+    private int y;
+
     public Point(int _x, int _y) {
         this.x = _x;
         this.y = _y;
-    }
-
-    @Override
-    public String toString() {
-        return String.format("(%s; %s)", x, y);
     }
 
     public boolean equals (Point point) {
@@ -30,5 +26,10 @@ public class Point {
         System.out.println("Point 2's position is " + pt2);
         System.out.println("Point1 == Point2: " + (pt1==pt2));
         System.out.println("Point1.equals(Point2): " + pt1.equals(pt2) + "\n");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%s; %s)", x, y);
     }
 }
